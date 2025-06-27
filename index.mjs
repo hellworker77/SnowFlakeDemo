0a1fd6dec793250bcd71a444017667d00aaf2872
FelFork <iljapuskov@gmail.com>
2025-06-27 02:02:36 +0200

    feat prepared demo

--- index.mjs
+++ index.mjs
@@ -1,6 +1,31 @@
-// @ts-nocheck
+import {SnowFlakeBuilder} from "./bundle.obf.js"
+
 const canvas = document.getElementById("canvas");
-const gfx = arenite.findOptimalRender(canvas)
-gfx.render(ctx => {
-    arenite.squareDrawer.draw(ctx, arenite.square)
-})
+const metrics = document.getElementById("debugOverlay");
+const builder = new SnowFlakeBuilder()
+
+builder
+    .useCanvas(canvas)
+    .useDpr()
+    .useEventBus()
+    .useOptimalRendering()
+    .useMetrics(metrics)
+    .useWebLoader()
+    .setUrlSchemaJson("schema/large.json")
+    .setUrlTicketsJson("tickets/large.json");
+
+let instance;
+
+builder.build()
+    .then(arenite => {
+        instance = arenite;
+        instance.run();
+    }).catch(error => {
+    console.error("Ошибка при сборке SnowFlake:", error);
+});
+
+window.addEventListener('beforeunload', (event) => {
+    if (instance) {
+        instance.stop?.();
+    }
+});
